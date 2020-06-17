d38e3a77e294b4bc49ced71c6b36f981db87a8dc
ParkJihye <wlgul817@gmail.com>
2020-06-17 14:30:28 +0900

    게시글 등록 만들기

--- src/main/java/com/boottest/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/boottest/book/springboot/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package com.boottest.book.springboot.service.posts;
 
 import com.boottest.book.springboot.domain.posts.Posts;
 import com.boottest.book.springboot.domain.posts.PostsRepository;
+import com.boottest.book.springboot.web.dto.PostsListResponseDto;
 import com.boottest.book.springboot.web.dto.PostsResponseDto;
 import com.boottest.book.springboot.web.dto.PostsSaveRequestDto;
 import com.boottest.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -35,5 +38,12 @@ public class PostsService {
         new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
